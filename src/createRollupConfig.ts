e5b21ac8d6836d8ece0c05f05dbc74f0a75eeb06
hb-seb <69623566+hb-seb@users.noreply.github.com>
2020-08-16 18:40:23 +0200

    Add regenerator-runtime and core-js to external, so the polyfills are included in the bundle

--- src/createRollupConfig.ts
+++ src/createRollupConfig.ts
@@ -59,7 +59,14 @@ export async function createRollupConfig(
     // Tell Rollup the entry point to the package
     input: opts.input,
     // Tell Rollup which packages to ignore
-    external: external,
+    external: (id: string) => {
+      // bundle in any polyfills as TSDX can't control whether polyfills are installed as deps
+      if (id.startsWith('regenerator-runtime') || id.startsWith('core-js')) {
+        return false;
+      }
+
+      return external(id);
+    },
     // Rollup has treeshaking by default, but we can optimize it further...
     treeshake: {
       // We assume reading a property of an object never has side-effects.
